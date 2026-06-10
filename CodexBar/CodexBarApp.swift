770957b3749df3b8e36eaa8f338cf4469e453c1f
Bob <zhang.zebedy@gmail.com>
2026-06-10 13:39:59 +0800

    Initial Commit

---
+++ CodexBar/CodexBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodexBarApp.swift
+//  CodexBar
+//
+//  Created by Bob on 2026-06-10.
+//
+
+import SwiftUI
+
+@main
+struct CodexBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodexBar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CodexBar
+//
+//  Created by Bob on 2026-06-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
